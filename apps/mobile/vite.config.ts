58c65fdd8f0bc3d36fb45c8686f26ff38fac0ba3
Sherlock Tang <280320889@qq.com>
2026-01-20 20:23:37 +0800

    fix: add pnpm store to vite fs allow list to resolve blank page

--- apps/mobile/vite.config.ts
+++ apps/mobile/vite.config.ts
@@ -1,9 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot(process.cwd()),
+				// allow serving files from the pnpm store
+				'D:/dev-storage/pnpm/store'
+			]
+		}
+	},
 
 	test: {
 		expect: { requireAssertions: true },
